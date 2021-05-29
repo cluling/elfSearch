a367fe059af944b7dd845fe5292ef8ffb75f622f
christian luling <ccluling0@gmail.com>
2021-05-29 15:55:06 -0400

    stack.h complete. Began work on stack.c

---
+++ stack.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+
+static struct {
+    size_t size = 0;
+    char *top = 0;
+    char *index = 0;
+}stack;
+
+void push(char *fname)
+{
+
+
+
+
+
+
+}
+
+void pop()
+{
+
+
+
+
+
+
+}
+
+char *next()
+{
+
+
+
+
+
+
+}
+
+void cleanup()
+{
+
+
+
+
+
+
+}
---
+++ stack.h
@@ -0,0 +1,8 @@
+#ifndef STACK_H
+#define STACK_H
+
+void push(char *fname);
+void pop();
+char *next();
+
+#endif
